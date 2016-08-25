4830e46957c1b53948f0aa8b35d2acd14c9b8a0e
Peter S <s@pramos.me>
2016-08-25 13:33:06 +0200

    Refactored voting into small components

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default React.createClass({
-    getPair: function() {
-        return this.props.pair || [];
-    },
-    isDisabled: function() {
-        return !!this.props.hasVoted;
-    },
-    render: function() {
-        return <div className="voting">
-            {this.getPair().map(entry =>
-                <button key={entry}
-                        onClick={() => this.props.vote(entry)}>
-                    <h1>{entry}</h1>
-                </button>
-            )}
+    render: function () {
+        return <div>
+            {this.props.winner ?
+                <Winner ref="winner" winner={this.props.winner} /> :
+                <Vote {...this.props} />}
         </div>;
     }
 });
